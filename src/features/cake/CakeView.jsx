f0b557c1311c2768d7dab3b109ecd86c4be3d742
friendly-misanthrope <nickthedevguy@gmail.com>
2024-07-05 23:57:39 -0700

    added click handlers to dispatch cake/ordered and cake/restocked actions

--- src/features/cake/CakeView.jsx
+++ src/features/cake/CakeView.jsx
@@ -1,15 +1,16 @@
 import { useSelector, useDispatch } from 'react-redux';
+import { ordered, restocked } from './cakeSlice';
 
 
 const CakeView = () => {
   const numCakes = useSelector((state) => state.cake.numCakes);
   const dispatch = useDispatch();
-  
+
   return (
     <div>
       <h2>Number of cakes: {numCakes}</h2>
-      <button>Order cake</button>
-      <button>Restock cake</button>
+      <button onClick={() => dispatch(ordered(1))}>Order cake</button>
+      <button onClick={() => dispatch(restocked(5))}>Restock cake</button>
     </div>
   );
 }
